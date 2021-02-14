7caad623aebc6716c76357a7796cfbcf9273520c
Mikhail Sokolov <debagger@gmail.com>
2021-02-14 16:53:03 +0500

    Correct typing for FSPath

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
+import {} from 'fs/promises';
+import { join } from 'path';
+
+function getDirent(path: string) {
+  return { asDir: () => path, asFile: () => 'file' };
+}
+
+type FSPathType = { (): ReturnType<typeof getDirent>; [key: string]: FSPathType };
+
+export const FSPath = function(path: string):FSPathType {
+  return new Proxy(() => getDirent(path), {
+    get: (_, key: string) => FSPath(join(path, key)),
+  }) as FSPathType;
 };
+
