d1730b7e8dd3c9dc07323b5f6bf09ccb66d2f94c
Mikhail Sokolov <debagger@gmail.com>
2021-02-28 07:55:38 +0500

    Add reducer for FSAsyncIterable

--- src/fs-async-iterable.class.ts
+++ src/fs-async-iterable.class.ts
@@ -36,6 +36,24 @@ export class FSAsyncIterable<T> implements AsyncIterable<T> {
   }
 
   /**
+   * Calls the specified callback function for all the input elements.
+   * The return value of the callback function is the accumulated result,
+   * and is provided as an argument in the next call to the callback function.
+   * @param callback A function that accepts up to four arguments.
+   * The reduce method calls the callbackfn function one time for each element in the array.
+   * Must return promise or be async.
+   * @param initialValue initialValue is used as the initial value to start the accumulation.
+   * The first call to the callbackfn function provides this value as an argument.
+   */
+  async reduce<U>(callback: (previousValue: U, currentValue: T) => Promise<U>, initialValue: U): Promise<U> {
+    let previousValue = initialValue;
+    for await (const item of this.iterable) {
+      previousValue = await callback(previousValue, item);
+    }
+    return previousValue;
+  }
+
+  /**
    * The forEach() method executes a provided function once for each array element
    * @param callback - async function (must return Promise).
    */
@@ -48,10 +66,9 @@ export class FSAsyncIterable<T> implements AsyncIterable<T> {
    *The toArray() method collect itertor items to array.
    */
   async toArray() {
-    const result: T[] = [];
-    for await (const item of this.iterable) {
-      result.push(item);
-    }
-    return result;
+    return await this.reduce(async (acc, item) => {
+      acc.push(item);
+      return acc;
+    }, [] as T[]);
   }
 }
